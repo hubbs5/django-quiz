516995df77a72732f6d56c5020b0b11b73f96dc0
Christian <christiandhubbs@gmail.com>
2023-02-27 17:07:12 -0600

    clean up views

--- src/apps/quizzes/urls.py
+++ src/apps/quizzes/urls.py
@@ -7,8 +7,7 @@ app_name = 'quizzes'
 urlpatterns = [
   path('', views.IndexView.as_view(), name='index'),
   path('<int:pk>/', views.QuizView.as_view(), name='quiz'),
-  path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-  # path('<int:question_id>/results/', views.ResultsView.as_view(), name='results'),
+  # path('<int:question_id>/', views.DetailView.as_view(), name='detail'),
   path('<int:question_id>/results/', views.results, name='results'),
   path('<int:question_id>/grade/', views.grade, name='grade'),
 ]
--- src/apps/quizzes/views.py
+++ src/apps/quizzes/views.py
@@ -35,19 +35,44 @@ def results(request, question_id):
 
 
 def grade(request, question_id):
+  # quiz = get_object_or_404(Quiz, pk=quiz_id)
   question = get_object_or_404(Question, pk=question_id)
+  num_questions = 1
   try:
     selected_choice = question.choice_set.get(pk=request.POST['choice'])
   except (KeyError, Choice.DoesNotExist):
     context = {'question': question,
                'error_message': "You didn't make a selection."}
-    return render(request, 'quizzes/detail.html', context=context)
+    return render(request, 'quizzes/results.html', context=context)
   else:
     selected_choice.selections += 1
     selected_choice.save()
+    points = 1 if selected_choice.correct else 0
+    score = f'{points / num_questions * 100:.1f}%' # Only checks one question at a time
     context = {
-      'choice': selected_choice
+      'question': question,
+      'choice': selected_choice,
+      'points': points,
+      'score': score,
     }
-    return render(request, 'quizzes/detail.html', context=context)
-    # return HttpResponseRedirect(
-    #   reverse('quizzes:results', args=(question.id,)))
+    return render(request, 'quizzes/results.html', context=context)
+
+
+def grade_quiz(request, quiz_id):
+  quiz = get_object_or_404(Quiz, pk=quiz_id)
+  if request.method == 'POST':
+    questions = quiz.question_set.all()
+    points = 0
+    for i, q in enumerate(questions):
+      # TODO: get individual choices if choice.correct = True then +1 to points
+      pass
+
+    score = points / (i + 1)
+
+    context = {
+      'quiz': quiz,
+      'points': points,
+      'score': score
+    }
+
+    return render(request, 'quizzes/results.html', context=context)
